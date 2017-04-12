89ec2d3df59fbe8746f8e200aa626f4584a4ca31
luiseduardo3004 <luiseduardo3004@gmail.com>
2017-04-12 01:26:44 -0500

    Initial Commit

---
+++ WeatherYahoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherYahoo
+//
+//  Created by Luis Santamaría on 4/12/17.
+//  Copyright © 2017 Santamaria Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
